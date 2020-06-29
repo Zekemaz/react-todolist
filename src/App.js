cec716e42170b9f12b23ad2d7ce9ed22088b2ac0
Gabe <desseresusinigabriel@gmail.com>
2020-06-29 19:44:56 +0200

    Add steps to follow to complete project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import './App.css';
 
+
+/**
+ * Clear project
+ * Create a Component folder within Src folder
+ * Create a ToDoForm.js file in Components
+ * Create a ToDoList.js file in Components
+ * 
+ * Display todos in a nice Material Design fashion
+ * Allow adding todos via input
+ * Delete todos
+ * 
+ */
+
 function App() {
   return (
     <div className="App">
