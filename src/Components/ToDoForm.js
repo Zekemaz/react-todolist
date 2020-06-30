8e73b4d097bfc4ade6a88f4a2d1453efb38db282
Gabe <desseresusinigabriel@gmail.com>
2020-06-30 11:49:28 +0200

    Setting up the form and methods

--- src/Components/ToDoForm.js
+++ src/Components/ToDoForm.js
@@ -3,11 +3,13 @@
  * Install material ui via 
     * $ npm install @material-ui/core
     * $ npm install @material-ui/icons
+    * $ npm install @material-ui/styles
+
  * 
- * Import React
+ * Import React && useState Hook
  * Import TextField from material ui
  * Create Form
- * Add onChange, onSubmit props to Textfield
+ * Add onChange, onSubmit props to Form
  * Import this component in your App.js.
  *
  * Clear input in ToDoForm.js.
@@ -15,9 +17,36 @@
  *
  */
 
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField'
 
+const ToDoForm = ({saveTodo}) => {
 
-const ToDoForm = () => {}
+    const [value, setValue] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        saveTodo(value);
+        setValue('');
+    }
+
+    const onChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    const textFieldProps = {
+        autoFocus: true,
+        maxLength: 50,
+        
+    }
+      
+    return(
+        <form className="todo-form" onSubmit={onSubmit}>
+            <TextField inputProps={textFieldProps}
+             value={value} onChange={onChange} variant="outlined" 
+             label="Need to do something?" color="secondary"/>
+        </form>
+    )
+}
 
 export default ToDoForm
